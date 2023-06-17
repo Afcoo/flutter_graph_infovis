42301bacc9b7f238f263b3692597558377a56402
Afcoo <afcoo0215@gmail.com>
2023-06-18 03:32:32 +0900

    data loading

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,13 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:csv/csv.dart';
 import 'package:fl_chart/fl_chart.dart';
+
+import 'dart:io';
+import 'dart:async';
 
 void main() {
   runApp(const MyApp());
@@ -31,6 +39,29 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  late List<List<dynamic>> data;
+
+  @override
+  void initState() {
+    super.initState();
+
+    debugPrint("Start Data Loading...");
+    loadData();
+  }
+
+  void loadData() async {
+    String csvText;
+    if (kIsWeb) {
+      final response = await http.get(Uri.parse("assets/assets/data.csv"));
+      csvText = utf8.decode(response.bodyBytes);
+    } else {
+      csvText = await File('assets/data.csv').readAsString();
+    }
+
+    data = const CsvToListConverter().convert(csvText);
+    debugPrint("Data Load Done");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,18 +69,24 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: BarChart(
-          BarChartData(
-              barGroups: [1, 2, 3, 4]
-                  .map((e) => BarChartGroupData(
-                        x: e,
-                        barRods: [BarChartRodData(toY: e.toDouble())],
-                      ))
-                  .toList()),
-          swapAnimationDuration: const Duration(microseconds: 150),
-          swapAnimationCurve: Curves.linear,
-        ),
+      body: Column(
+        children: [
+          SizedBox(
+            width: 800,
+            height: 600,
+            child: BarChart(
+              BarChartData(
+                  barGroups: [1, 2, 3, 4]
+                      .map((e) => BarChartGroupData(
+                            x: e,
+                            barRods: [BarChartRodData(toY: e.toDouble())],
+                          ))
+                      .toList()),
+              swapAnimationDuration: const Duration(microseconds: 150),
+              swapAnimationCurve: Curves.linear,
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
